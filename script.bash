f8a9912a5cfd17cc280f695bdc44311bda8db0d5
marti <140777358+MartiNogueira@users.noreply.github.com>
2024-11-15 00:01:31 -0300

    Update script.bash

--- script.bash
+++ script.bash
@@ -2,7 +2,8 @@
 curl https://gist.githubusercontent.com/sebiglesias/ea2faa92f4b25a79f811104584e91efb/raw/02378f041ae64d3d021031efeb1572cbddfc2fc7/test-web-server-log.txt
 touch log.txt
 ls | grep -w "log.txt" #verifica si existe log.txt
-https://gist.githubusercontent.com/sebiglesias/ea2faa92f4b25a79f811104584e91efb/raw/02378f041ae64d3d021031efeb1572cbddfc2fc7/test-web-server-log.txt >> log.txt
+
+curl https://gist.githubusercontent.com/sebiglesias/ea2faa92f4b25a79f811104584e91efb/raw/02378f041ae64d3d021031efeb1572cbddfc2fc7/test-web-server-log.txt >> log.txt
 cat log.txt | grep “ 200 “ >> ok.txt
 cat log.txt | grep “ 500 “ >> error.txt
 
